50a5718d570bd567d4097a1a1a3073246d5f88e0
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-05-04 06:53:27 +0530

    DarkMusicBot

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DARKEMPIRESL/DarkMusicBot",
+    "https://github.com/TEAM-DARK-MUSIC/DarkMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
@@ -66,10 +66,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", None
-)  # Example:- https://t.me/TheYukki
+)  # Example:- https://t.me/SLBotOfficial
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", None
-)  # Example:- https://t.me/YukkiSupport
+)  # Example:- https://t.me/darkmusicsupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
@@ -134,7 +134,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(
 # Chceckout https://www.gbmb.org/mb-to-bytes  for converting mb to bytes
 
 
-# You'll need a Pyrogram String Session for these vars. Generate String from our session generator bot @YukkiStringBot
+# You'll need a Pyrogram String Session for these vars. Generate String from our session generator bot @darkStringGenBot
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
@@ -142,14 +142,10 @@ STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
 
-     
-     
 #         ___   ___   ___  __ __  __  _____  _______________  ___  ____  ______
 #        / _ \/ _ | / _ \/ //_/ /  |/  / / / / __/  _/ ___/ / _ )/ __ \/_  __/
 #       / // / __ |/ , _/ ,<   / /|_/ / /_/ /\ \_/ // /__  / _  / /_/ / / /   
 #      /____/_/ |_/_/|_/_/|_| /_/  /_/\____/___/___/\___/ /____/\____/ /_/    
-     
-
 
 ### DONT TOUCH or EDIT codes after this line
 BANNED_USERS = filters.user()
